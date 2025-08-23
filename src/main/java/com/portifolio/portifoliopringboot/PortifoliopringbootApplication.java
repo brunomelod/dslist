01b5c2567de7642ae78ef974b036571299cc51d5
Bruno Melo <brunomelod@gmail.com>
2025-08-23 15:52:46 -0300

    Project Created

---
+++ src/main/java/com/portifolio/portifoliopringboot/PortifoliopringbootApplication.java
@@ -0,0 +1,13 @@
+package com.portifolio.portifoliopringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortifoliopringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortifoliopringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portifolio/portifoliopringboot/PortifoliopringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portifolio.portifoliopringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortifoliopringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
