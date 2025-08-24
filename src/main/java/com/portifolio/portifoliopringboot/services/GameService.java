02d2fb81a72df387713aa2515f0b18a3e72b7585
Bruno Melo <brunomelod@gmail.com>
2025-08-24 11:01:04 -0300

    Get game by id

--- src/main/java/com/portifolio/portifoliopringboot/services/GameService.java
+++ src/main/java/com/portifolio/portifoliopringboot/services/GameService.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.portifolio.portifoliopringboot.dto.GameDTO;
 import com.portifolio.portifoliopringboot.dto.GameMinDTO;
+
+import org.springframework.transaction.annotation.Transactional;
+
 import com.portifolio.portifoliopringboot.Entities.Game;
 import com.portifolio.portifoliopringboot.Repository.GameRepository;
 
@@ -15,6 +19,14 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        GameDTO dto = new GameDTO(result);
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
